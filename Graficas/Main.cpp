93ff30d77d08c024f9a8d6cec71cb0a884039f79
DAPH\Daphne <daph.san22@gmail.com>
2017-09-04 10:23:16 -0500

    En Clase: triangulos con color

--- Graficas/Main.cpp
+++ Graficas/Main.cpp
@@ -12,6 +12,71 @@ Autor:					A01169892 Daphne Sánchez Flores
 #include<iostream>
 #include<GL/glew.h>
 #include<GL/freeglut.h>
+#include <glm/glm.hpp> //libreria de matemmaticas para graficas
+#include <vector> //listas= arreglo pero mas boonito
+
+//identificador del manager de VBO's para 1 geometria para un conjunto de vertices
+GLuint vao;
+
+//solo se configura una vez en el programa, función que inicializa toda
+//la memoria del programa
+void Initialize()
+{	//vamos a crear una lista que almacene las posiciones
+	//en 2 dimensiones de un triangulo.
+	//Esto es CPU y RAM
+	std::vector<glm::vec2> positions;
+
+	positions.push_back(glm::vec2(-1.0f, -1.0f));
+	positions.push_back(glm::vec2(1.0f, -1.0f));
+	positions.push_back(glm::vec2(0.0f, 1.0f));
+
+	//vamos a crear una ista para almacenar colores RGB
+	std::vector<glm::vec3> colors; //<tipo de vector: 3 datos> nombre de la lista;
+	//RGB EN FLOATS
+	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f)); //rojo
+	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f)); //verde
+	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f)); //azul
+
+	//Creamos un VAO y almacenamos el id en la variable vao.
+	//este es el manager de VBO's
+	glGenVertexArrays(1, &vao); //se crea 1 y se guarda en &vao (sí, con "&")
+	//todos los VBO's que se creen a partir de este punto y hasta
+	//que desactivemos este bound, se van a asociar a este manager.
+	glBindVertexArray(vao);
+
+	//equivale a un entero sin signo. ahi guardamos el
+	//identificador del VBO de posiciones
+	GLuint positionsVBO;
+	//creamos un BO y almacenamos el identificador en la variable positions VBO
+	glGenBuffers(1, &positionsVBO);
+
+	//le decimos a OpenGL que lo que queremos utilizar como un atributo.
+	glBindBuffer(GL_ARRAY_BUFFER, positionsVBO);
+
+	//Asignamos los datos al buffer y los mandamos a la tarjeta de video
+	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2)* positions.size(), positions.data(), GL_STATIC_DRAW);
+
+	//Quiero activar el atributo con indice 0 en la tarjeta de video
+	glEnableVertexAttribArray(0);
+	
+	//configuramos el atributo con indice 0 en la tarjeta de video
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+	//Desactivamos el uso 
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	GLuint colorsVBO;
+	glGenBuffers(1, &colorsVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, colorsVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3)*colors.size(), colors.data(), GL_STATIC_DRAW);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	//ya no quiero usar el VAO. ya no se asociaran mas VBOs a este vao
+	glBindVertexArray(0);
+
+}
 
 void MainLoop()
 {
@@ -19,19 +84,22 @@ void MainLoop()
 	//inicio de un nuevo frame
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	//WARNING!!!! esto es OpenGl clásico y no lo vamos a volver a usar
-	//en todo el semestre.
-	glBegin(GL_TRIANGLES);
+	//TO DO: hacer el render
 
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex2f(-1.0f, -1.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex2f(1.0f, -1.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex2f(0.0f, 1.0f);
+	//PASO 1  AUN NO LO PODEMOS HACER!!
 
-	glEnd();
-	//FIN DEL WARNING
+
+	//PASO2: ACTIVAR UN vao
+	glBindVertexArray(vao);
+
+	//PASO 3: DIBUJAMOS
+	glDrawArrays(GL_TRIANGLES, 0, 3); //(que usamos para dibujar, desde qué lugar de la lista, cuantos elementos)
+
+	//PASO 4: DESACTIVAMOS EL VAO
+	glBindVertexArray(0);
+
+	//PASO 5: AUN NO LO PODEMOS HACER!
+
 
 	//Intercambiar los buffer (el que se estaba
 	//rendereando con el que se estaba mostrando.
@@ -45,8 +113,10 @@ int main(int argc, char * argv[])
 	// usamos freeglut para crear una ventana donde vamos a dibujar
 	glutInit(&argc, argv);
 	
+
 	//teenmos que informar que queremos trabajar con el pipline clasico (OpenGL viejito)
-	glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+	// programable (OpenGl moderno)
+	glutInitContextProfile(GLUT_CORE_PROFILE);
 
 	//freeglut nos permite configurar eventos que ocurren en la ventana
 	//uno de ellos es cuando alguien cierra la venta
@@ -77,6 +147,9 @@ int main(int argc, char * argv[])
 	//configurar OpenGl
 	//Este es el color por default del buffer de color en el framebuffer.
 	glClearColor(1.0f, 1.0f, 0.5f,1.0f);
+
+	//configurar la memoria que la aplicacion va a necesitar
+	Initialize();
 
 	//iniciamos la aplicacion gráfica.
 	//el main se pausará en esta linea hasta que se cierre
